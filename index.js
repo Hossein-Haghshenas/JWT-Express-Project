5966e101be1afcb53cbfdb9bf713fd4d5268b087
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-04 13:24:16 +0430

    Create main app

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const auth = require("./routes/auth");
 const Port = 5000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
   res.send("hi im working");
