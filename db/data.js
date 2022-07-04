e68de0dd02dedf3eed42bfa037ddaf3878bb5ca0
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-04 20:41:40 +0430

    Create some data

--- db/data.js
+++ db/data.js
@@ -3,6 +3,44 @@ const users = [
     email: "hossein2020@gmail.com",
     password: "lslslslslslls",
   },
+  {
+    email: "hosehose@gmail.com",
+    password: "5h5h5h",
+  },
 ];
 
-module.exports = users;
+const publicPosts = [
+  {
+    title: "Free Tipe on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tipe on Development",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tipe on Development",
+    content: "These are some tips",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Free Tipe on 56 ",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tipe on 56",
+    content: "These are some tips",
+  },
+  {
+    title: "Free Tipe on 56",
+    content: "These are some tips",
+  },
+];
+
+module.exports = {
+  users,
+  publicPosts,
+  privatePosts,
+};
